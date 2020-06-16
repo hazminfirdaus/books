887f734aba372a7f0c64a4b4c64dfdac72e09cab
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-06-16 15:14:12 +0800

    edited

--- resources/views/publishers/index.blade.php
+++ resources/views/publishers/index.blade.php
@@ -4,26 +4,24 @@
 
 @section('content')
 
-<h1>Publishers Index</h1>
+    <h1>Publishers Index</h1>
 
-@foreach($publishers as $p)
+        @foreach($publishers as $p)
 
-  <h2>{{ $p->title }}</h2>
+            <h2>{{ $p->title }}</h2>
   
-  @component('publishers.unordered-list')
+            @component('publishers.unordered-list')
 
-  <ul>
+                <ul>
+                    @foreach($p->books as $b)
+                        <li>{{ $b->title }}</li>
+                    @endforeach
+                </ul>
 
-    @foreach($p->books as $b)
-      <li>{{ $b->title }}</li>
-    @endforeach
+            @endcomponent
 
-  </ul>
+            <a href="publishers/{{ $p->id }}">Read more...</a>
 
-  @endcomponent
-
-    <a href="publishers/{{ $p->id }}">Read more...</a>
-
-@endforeach
+        @endforeach
 
 @endsection
